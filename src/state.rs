56c5a00cf258c2ec6819d3e8928d8ea02ad3671a
WenZheng Li <me@wenzheng.li>
2025-02-13 23:59:45 -1000

    added state tests

--- src/state.rs
+++ src/state.rs
@@ -1,24 +1,14 @@
 use crate::*;
 
-use alloc::boxed::Box;
-
 pub enum State {
     Running(future::BoxedLocal<State>),
     Success,
     Failure,
 }
 
-/*
-impl<FB: Future<Output = bool>, FS: Future<Output = State>> From<FB> for FS {
-    fn from(b: FS) -> FS {
-        b.map(State::Success)
-    }
-}
-*/
-
 impl<F: Future<Output = State> + 'static> From<F> for State {
     fn from(f: F) -> Self {
-        Self::Running(Box::pin(f))
+        Self::Running(alloc::boxed::Box::pin(f))
     }
 }
 
@@ -82,76 +72,40 @@ mod tests {
     use super::*;
     use future::block_on;
 
-    #[instrument]
-    async fn state0() -> State {
-        success().into()
-    }
-
-    #[instrument]
-    async fn state1() -> State {
-        state0().into()
-    }
-
-    #[instrument]
-    async fn state2() -> State {
-        select!(
-            state1().when(|| state1().eval()),
-            success().when(|| success().eval()),
-            result_when(false, false, || state1().eval()),
-        )
-        .into()
-    }
-
-    #[instrument]
-    async fn dstate0() -> State {
-        state2().into()
-    }
-
-    #[instrument]
-    async fn dstate1() -> State {
-        dstate1().into()
-    }
-
-    #[instrument]
-    async fn dstate2() -> State {
-        dstate3().into()
-    }
-
-    #[instrument]
-    async fn dstate3() -> State {
-        dstate3().into()
-    }
-
-    #[instrument]
-    async fn dstated() -> State {
-        select!(failure().when(|| state1().eval()), success(), failure(),).into()
-    }
-
-    #[instrument]
-    async fn state_a() -> State {
-        state_b().into()
-    }
-
-    #[instrument]
-    async fn state_b() -> State {
-        state_a().into()
-    }
-
-    /*
-        #[test]
-        fn good_test() {
-            /*
-            let _ = tracing_subscriber::fmt()
-                .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-                .with_span_events(
-                    tracing_subscriber::fmt::format::FmtSpan::NEW
-                        | tracing_subscriber::fmt::format::FmtSpan::CLOSE,
-                )
-                .with_target(false)
-                .try_init();
-            */
-            assert!(block_on(dstated().eval()));
-            assert!(block_on(state_a().eval()));
+    #[test]
+    fn state_eval() {
+        async fn a() -> State {
+            success().into()
         }
-    */
+
+        async fn b() -> State {
+            a().into()
+        }
+
+        async fn c() -> State {
+            b().into()
+        }
+
+        assert!(block_on(c().eval()));
+    }
+
+    #[test]
+    fn state_when() {
+        async fn x() -> State {
+            select!(x().when(|| ready(false)), success().when(|| ready(true)),).into()
+        }
+        assert!(block_on(x().eval()));
+    }
+
+    #[test]
+    fn result_when_eval() {
+        async fn x() -> State {
+            select!(
+                x().when(|| ready(false)),
+                result_when(true, false, || ready(true)),
+            )
+            .into()
+        }
+        assert!(block_on(x().eval()));
+    }
 }
